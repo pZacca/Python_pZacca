ac47fcb259457875bce6bb95c740bd7c5e51b2ec
Pedro Zaccaria <pedrozaccaria@gmail.com>
2022-02-28 23:12:10 -0300

    .

--- Conjunto/Curso Python/Projetinhos/Desafio4 VALIDADOR DE CPF/Desafio4.py
+++ Conjunto/Curso Python/Projetinhos/Desafio4 VALIDADOR DE CPF/Desafio4.py
@@ -18,8 +18,62 @@ Digito 1 = 0          #   Digito 2 = 9
 """
 
 print('Seja bem-vindo ao validador de CPF! Será que o seu é válido mesmo?\n')
+
+
 while True:
     while True:  # Validando as entradas para verificar se o CPF contém exatos 11 e apenas dígitos.
+        cpf = input('Insira seu CPF (apenas números): ')
+
+
+        if not cpf.isnumeric():
+            print('Favor digitar apenas números.\n')
+            continue
+
+
+        elif not len(cpf) == 11:
+            print('O CPF deve conter 11 dígitos.\n')
+            continue
+
+
+        else:
+            break
+
+
+    i = 0
+
+
+    for n, m in enumerate(range(10, 1, -1)):
+        i += int(cpf[n])*m
+
+
+    dig_1 = 0 if 11-(i%11)>9 else 11-(i%11)
+    cpfnovo = cpf[0:9] + str(dig_1)
+    i = 0
+
+
+    for n, m in enumerate(range(11, 1, -1)):
+        i += int(cpfnovo[n]) * m
+
+
+    dig_2 = 0 if 11-(i%11)>9 else 11-(i%11)
+    cpfnovo = cpfnovo + str(dig_2)
+
+
+    if not cpfnovo == cpf:
+        print('Seu CPF não é valido, tente novamente.\n')
+        continue
+
+
+    else:
+        print('Seu CPF é valido!')
+        break
+
+
+# Método realizado pelo professor, mais linhas porém mais leveza no código
+'''
+print('Seja bem-vindo ao validador de CPF! Será que o seu é válido mesmo?\n')
+while True:
+    while True:
         cpf = input('Insira seu CPF (apenas números): ')
         if not cpf.isnumeric():
             print('Favor digitar apenas números.\n')
@@ -29,24 +83,25 @@ while True:
             continue
         else:
             break
-
-    i = 0
-    for n, m in enumerate(range(10, 1, -1)):
-        i += int(cpf[n])*m
-
-    dig_1 = 0 if 11-(i%11)>9 else 11-(i%11)
-    cpfnovo = cpf[0:9] + str(dig_1)
-
-    i = 0
-    for n, m in enumerate(range(11, 1, -1)):
-        i += int(cpfnovo[n]) * m
-
-    dig_2 = 0 if 11-(i%11)>9 else 11-(i%11)
-    cpfnovo = cpfnovo + str(dig_2)
-
-    if not cpfnovo == cpf:
-        print('Seu CPF não é valido, tente novamente.\n')
-        continue
-    else:
+    novocpf = cpf[:-2]
+    reverso = 10
+    total = 0
+    for index in range(19):
+        if index > 8:
+            index -= 9
+        total += int(novocpf[index]) * reverso
+        reverso -= 1
+        if reverso < 2:
+            reverso = 11
+            d = 11 - (total % 11)
+            if d> 9:
+                d = 0
+            total = 0
+            novocpf += str(d)
+    if cpf == novocpf:
         print('Seu CPF é valido!')
         break
+    else:
+        print('Seu CPF não é valido, tente novamente.\n')
+        continue
+'''
